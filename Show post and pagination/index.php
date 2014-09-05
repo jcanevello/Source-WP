2458aeed7736991be163c391b7ef468640a68020
jean carlo canevello salazar <jean.canevello@gmail.com>
2014-09-05 15:05:47 -0500

    index

---
+++ Show post and pagination/index.php
@@ -0,0 +1,45 @@
+﻿<?php 
+/*
+ * 	$paged
+ *	 Indica la paginación actual
+ */
+$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+
+
+/*
+ * 	$arg
+ *	 Variable que contiene las condiciones de la consulta
+ *	 Se debe de pasar la variable $paged para obtener los post de la paginación
+ */
+$arg = array(
+	'paged' => $paged
+	);
+
+
+/*
+ * 	$wp_query
+ *	 Variable que contiene a todos los post
+ */
+
+$wp_query = new WP_Query($arg);
+
+/*
+ * Se realiza el loop de los post
+ */
+
+if($wp_query->have_posts())
+{
+	while($wp_query->have_posts())
+	{
+		$wp_query->have_posts();
+		echo the_title();
+	}
+}
+//-Termina Loop-//
+
+/*
+	Llamamos a la función pagination
+*/
+pagination($wp_query);
+
+?>
